647521576c55003c8f29442cddc31991749baeb2
Timothy Liu <chbrahmsliu@gmail.com>
2016-11-13 17:27:21 -0500

    TST/MNT: modify based on @CJ-Wright's review. write first test

--- xpdan/tests/test_tiff_exporter.py
+++ xpdan/tests/test_tiff_exporter.py
@@ -1,12 +1,26 @@
-from xpdan.glbl import an_glbl
+import os
+import tempfile
+from tifffile import imread
+from xpdan.tests.conftest import img_size
 from xpdan.callbacks_core import XpdAcqLiveTiffExporter
 
 # standard config
-
-template = '/direct/XF28ID1/pe2_data/xpdUser/tiff_base/{start.sample_name}'
+base = tempfile.mkdtemp()
+template = os.path.join(base, 'xpdUser/tiff_base/')
 data_fields = ['temperature', 'diff_x', 'diff_y', 'eurotherm'] # known devices
 
-def test_tiff_export_smoke_test(exp_db):
+def test_tiff_export(exp_db):
     tif_export = XpdAcqLiveTiffExporter('pe1_image', template, data_fields,
                                         overwrite=True, db=exp_db)
-    an_glbl.exp_db.process(exp_db[-1], tif_export)
+    exp_db.process(exp_db[-1], tif_export)
+    # make sure files are sasved
+    for fn in tif_export.filenames:
+        assert os.path.isfile(fn)
+    # confirm image is the same as input
+    for fn in tif_export.filenames:
+        img = imread(fn)
+        assert img.shape == next(img_size)
+        # logic defined in insert_img. after successful dark_sub array==0
+        # TODO: update this logic when we are ready for
+        # fs-integrated-Reader
+        assert np.all(img == 0)
